8f1e35bcdb5ff967ee352b86815d8113001fcf1d
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-07-23 00:04:23 +0900

    add update_readme.py

--- README.rst
+++ README.rst
@@ -2,9 +2,6 @@
 Tips
 ====
 
-Index
-=====
-
 * `Use Old GitHub Header <2015-06-21-use-old-github-header/README.rst>`_
 * `SSH Forwarding and ssh_cofig file <2015-06-28-ssh-forwarding-config/README.rst>`_
 * `Usage of git_svn <2015-06-28-usage-of-git-svn/README.rst>`_
--- update_readme.py
+++ update_readme.py
@@ -21,16 +21,27 @@ def get_rst_h1(filename):
     return title
 
 
-def main():
+def get_index():
     files = os.listdir('.')
     pattern = re.compile('^\d{4,4}-\d{1,2}-\d{1,2}-.*')
-
     for file_ in files:
         if not (os.path.isdir(file_) and pattern.match(file_)):
             continue
         filename = os.path.join(file_, 'README.rst')
         title = get_rst_h1(filename)
-        print('* `{label} <{url}>`_'.format(label=title, url=filename))
+        line = '* `{label} <{url}>`_'.format(label=title, url=filename)
+        yield line
+
+
+def main():
+    index_list = get_index()
+
+    lines = ['====', 'Tips', '====']
+    lines.append('')  # space
+    lines.extend(index_list)
+    lines.append('')  # new lines at EOF
+    with open('README.rst', 'w') as f:
+        f.write('\n'.join(lines))
 
 
 if __name__ == '__main__':
